1aea0bfc240650c6bd8b2558e35dd196d58dc297
Brandon Man <75937508+brandon-qq@users.noreply.github.com>
2023-10-18 10:38:12 +0200

    Added Questions and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { ProverbExplorer } from "./components/ProverbExplorer";
+import { Questions } from "./components/Questions";
+import { Navbar } from "./components/Navbar";
 function App() {
 
 
   return (
     <div className="h-screen">
       <div className="h-20 mb-3 p-3 rounded-b-lg bg-[#bad7f2]">Hi</div>
+    <Navbar />
+    <Questions />
     <ProverbExplorer />
       </div>
     
@@ -12,4 +16,3 @@ function App() {
 }
 
 export default App;
-
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+    return (
+        <div>
+        <div className="flex h-20 mb-3 rounded-b-lg bg-[#bad7f2]">
+            <div className="p-6">
+            Proverb Explorer
+            </div>
+            <div>
+            dd
+            </div>
+        </div>
+        </div>
+    );
+}
